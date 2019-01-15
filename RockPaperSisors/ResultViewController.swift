1fcfe3ac445ba297943260dadfc2805bae13d76e
Israel Toledo <j.israel.toledo@gmail.com>
2019-01-15 11:41:24 -0600

    first commit

--- RockPaperSisors/ResultViewController.swift
+++ RockPaperSisors/ResultViewController.swift
@@ -11,11 +11,73 @@ import UIKit
 
 class ResultViewController: UIViewController {
   
+  @IBOutlet weak var labelUserShot: UILabel!
+  
+  @IBOutlet weak var lblMachineShot: UILabel!
+  @IBOutlet weak var btnResult: UIButton!
   var userShot : String?
   
+  @IBAction func goBack(_ sender: Any) {
+    print ("goback")
+    performSegueToReturnBack()
+  }
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    labelUserShot.text = userShot
+    let number = Int(arc4random_uniform(3))
+    print ("number \(number)")
+    
+    if (number == 0)
+    {
+      lblMachineShot.text = "rock"
+      if(userShot=="rock"){
+        print ("rock")
+        btnResult.setBackgroundImage(UIImage(named: "itsATie.png"), for: UIControlState.normal)
+      }
+      if (userShot == "paper"){
+        print ("paper")
+        btnResult.setBackgroundImage(UIImage(named: "PaperCoversRock.jpg"), for: UIControlState.normal)
+      }
+      if (userShot=="sisors"){
+        print ("sisors")
+        btnResult.setBackgroundImage(UIImage(named: "RockCrushesScissors.jpeg"), for: .normal)
+      }
+    }
+    if (number == 1)
+    {
+      lblMachineShot.text = "paper"
+      if(userShot=="rock"){
+        print ("rock")
+        btnResult.setBackgroundImage(UIImage(named: "PaperCoversRock.jpg"), for: UIControlState.normal)
+      }
+      if (userShot == "paper"){
+        print ("paper")
+        btnResult.setBackgroundImage(UIImage(named: "itsATie.png"), for: UIControlState.normal)
+        
+      }
+      if (userShot=="sisors"){
+        print ("sisors")
+        btnResult.setBackgroundImage(UIImage(named: "ScissorsCutPaper.jpg"), for: .normal)
+      }
+    }
+    if (number == 2)
+    {
+      lblMachineShot.text = "scissors"
+      if(userShot=="rock"){
+        print ("rock")
+        btnResult.setBackgroundImage(UIImage(named: "RockCrushesScissors.jpeg"), for: .normal)
+      }
+      if (userShot == "paper"){
+        print ("paper")
+        btnResult.setBackgroundImage(UIImage(named: "ScissorsCutPaper.jpg"), for: .normal)
+      }
+      if (userShot=="sisors"){
+        print ("sisors")
+        btnResult.setBackgroundImage(UIImage(named: "itsATie.png"), for: UIControlState.normal)
+        
+      }
+    }
+    
   }
   
   override func didReceiveMemoryWarning() {
@@ -26,3 +88,12 @@ class ResultViewController: UIViewController {
   
 }
 
+extension UIViewController {
+  func performSegueToReturnBack()  {
+    if let nav = self.navigationController {
+      nav.popViewController(animated: true)
+    } else {
+      self.dismiss(animated: true, completion: nil)
+    }
+  }
+}
--- RockPaperSisors/ViewController.swift
+++ RockPaperSisors/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var sisors: UIButton!
+  @IBOutlet weak var rock: UIButton!
+  @IBOutlet weak var paper: UIButton!
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,21 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    print (segue.destination)
+    if segue.identifier == "rockSegue" {
+      let resultController = segue.destination as! ResultViewController
+      resultController.userShot = "rock"
+    }
+    if segue.identifier == "paperSegue" {
+      let resultController = segue.destination as! ResultViewController
+      resultController.userShot = "paper"
+    }
+    if segue.identifier == "sisorsSegue" {
+      let resultController = segue.destination as! ResultViewController
+      resultController.userShot = "sisors"
+    }
+  }
 
 }
 
