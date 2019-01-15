2e2892f324ee931396a70b3f2db8cf45d4d3b133
Israel Toledo <j.israel.toledo@gmail.com>
2019-01-15 11:41:15 -0600

    first commit

---
+++ RockPaperSisors/ResultViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ResultViewcontroller.swift
+//  RockPaperSisors
+//
+//  Created by Alfonso Balbuena on 10/10/18.
+//  Copyright © 2018 Vanglar. All rights reserved.
+//
+
+import UIKit
+
+
+class ResultViewController: UIViewController {
+  
+  var userShot : String?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+}
+
